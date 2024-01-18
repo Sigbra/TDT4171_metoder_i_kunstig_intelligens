e29e27bf128f6acc55fff7b37fde648251cfa743
Sigurd von Brandis <sigurdvbrandis@gmail.com>
2024-01-18 01:45:11 +0100

    Assignment complete

--- Assignment1_TDT4171.py
+++ Assignment1_TDT4171.py
@@ -49,14 +49,70 @@ def simulate(num_simulations, starting_coins):
     return mean_plays, median_plays
 
 
-num_simulations = 1000
+num_simulations = 10000
 starting_coins = 10
-
 mean_plays, median_plays = simulate(num_simulations, starting_coins)
 
 print("Exercise 2c: slot machine")
 print(f"Mean: {mean_plays}")
-print(f"Median: {median_plays}")
+print(f"Median: {median_plays}\n")
 
-#Exercise 3 Part 1a
+#Exercise 3 Part 1 a 
+
+def probability_of_birthday_overlapp(N, sim_num =10000):
+    overlapp_count = 0
+    
+    for i in range(sim_num):
+        birthdays = [random.randint(1,365) for j in range(N)]
+
+        if len(birthdays) != len(set(birthdays)):
+            overlapp_count = overlapp_count + 1
+    
+    probability = overlapp_count/sim_num
+    return probability
+
+N=10
+probability = probability_of_birthday_overlapp(N)
+
+print("Exercise 3 part 1 a: probability_of_birthday_overlapp")
+print(f"Choosen N:{N}, Chance: {probability}\n")
+
+#Exercise 3 part 1 b
+
+def find_smallest_N_where_probability_is_50():
+    smallest_N = np.infty
+    for N in range(10,50):
+        probability = probability_of_birthday_overlapp(N)
+        if probability>=0.5 and N < smallest_N:
+            smallest_N = N
+
+    return smallest_N
+
+smallest_N = find_smallest_N_where_probability_is_50()
+
+print("Exercise 3 part 1 b: find_smallest_N_where_probability_is_50")
+print(f"smallest N: {smallest_N}\n")
+
+#Exercise part 2 a
+
+def find_N_needed_for_birthday_every_day():
+    birthdays_covered = set()
+    group_size = 0
+
+    while len(birthdays_covered) < 365:
+        birthdays_covered.add(random.randint(1, 365))
+        group_size = group_size + 1
+    
+    return group_size
+
+sim_num = 10000
+total_group_size = sum(find_N_needed_for_birthday_every_day() for i in range(sim_num))      
+average_group_size = total_group_size / sim_num
+
+print("Exercise 3 part 2 a: find_N_needed_for_birthday_every_day")
+print(f"Average simulated group_size needed: {average_group_size}\n")
+                    
+
+
+
 
